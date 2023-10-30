ea371c08b49e2032351ffba16d12976c683357a3
janvierjr <bernie@myheartsleeve.com>
2023-10-30 18:18:47 -0500

    create parallax components with h1 motion and update scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
 import './App.scss';
 import Navbar from './components/navbar/Navbar';
 import Hero from './components/hero/Hero';
+import Parallax from './components/parallax/Parallax';
 
 const App = () => {
   return (
     <div>
       <section id='Home'>
         <Navbar />
-        <Hero/>
+        <Hero />
       </section>
-      <section id='Services'>Parallax</section>
+      <section id='Services'>
+        <Parallax type='services'/>
+      </section>
       <section>Services</section>
-      <section id='Portfolio'>Parallax</section>
+      <section id='Portfolio'>
+        <Parallax type='portfolio'/>
+      </section>
       <section>PortfolioDemo1</section>
       <section>PortfolioDemo2</section>
       <section>PortfolioDemo3</section>
---
+++ src/components/parallax/Parallax.jsx
@@ -0,0 +1,41 @@
+import './Parallax.scss';
+import { motion, useScroll, useTransform } from 'framer-motion';
+import {useRef} from 'react';
+const Parallax = ({ type }) => {
+
+  const ref = useRef()
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ['start start', 'end start']
+  });
+
+  const yBackground = useTransform(scrollYProgress, [0, 1], ['0%', '200%']);
+  const yOpacity = useTransform(
+    scrollYProgress,
+    // Map scrollYProgress from these values:
+    [0, .45],
+    // Into these values:
+    [1, 0]
+  );
+
+  return (
+    <div
+      className='parallax-container'
+      ref={ref}
+      style={{
+        background:
+          type === 'services'
+            ? 'linear-gradient(180deg, var(--purple), var(--darker))'
+            : 'linear-gradient(180deg, var(--purple), var(--dark-main))',
+      }}
+    >
+      <motion.h1 style={{ y: yBackground, opacity: yOpacity }}>
+        {type === 'services' ? 'Services We provide' : 'See Our Work'}
+      </motion.h1>
+      <motion.div className='stars'></motion.div>
+      <motion.div className='muppets-group'></motion.div>
+    </div>
+  );
+};
+
+export default Parallax;
